d6670be410da25fedfe1faf748bc727c7278ad17
Stephen Florian <stephenflorian@Stephens-MacBook-Pro.local>
2021-04-12 05:37:29 -0600

    details

--- src/pages/Where.js
+++ src/pages/Where.js
@@ -4,26 +4,32 @@ import SlidingText from '../components/sliding-text/SlidingText'
 import rcmHeader from '../images/running-creek.png'
 
 const Where = () => {
+
+  const DirectionsWebsite = () => {
+    return <h3 className='directionsWebsite'>
+      <a href='https://goo.gl/maps/duh8kQRKXxUJqQCo8' target='_blank'>Directions</a>
+      <span>|</span>
+      <a href='http://runningcreekmanor.com/' target='_blank'>Website</a>
+    </h3>
+  }
+
   return <div className='columnContainer' style={{margin: '10px 0'}}>
-    {/* <SlideUpHeaderImage 
-      backgroundImage={rcmHeader}
-      text='Running Creek Manor'
-    /> */}
     <SlidingText 
       tag='h1'
       text='Running Creek Manor'
       delay='0'
     />
     <SlidingText 
-      tag='h1'
-      text='Running Creek Manor'
-      delay='5'
+      tag='p'
+      text='Information'
+      delay='3'
     />
     <SlidingText 
-      tag='h1'
-      text='Running Creek Manor'
-      delay='8'
-    />
+      tag='component'
+      delay='5'
+    >
+      <DirectionsWebsite />
+    </SlidingText>
   </div>
 }
 
